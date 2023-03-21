1a864cb3254dd8f045007d2e618f9e2877104da8
Timothy Bailey <mail@dispossible.com>
2023-03-21 21:21:12 +0000

    Initial commit of new keymap

---
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/dispossible/keymap.c
@@ -0,0 +1,175 @@
+/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "math.h"
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+enum custom_layers {
+    _BL,
+    _FL,
+};
+
+// Use this tool to figure out what the keycodes are called
+// https://config.qmk.fm/#/gmmk/gmmk2/p96/ansi/LAYOUT
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap _BL: Base Layer (Default Layer) */
+  [_BL] = LAYOUT(
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NUM,   KC_HOME,  KC_END,   KC_PSCR,  KC_INS,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,   KC_PSLS,  KC_PAST,  KC_PMNS,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
+    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_P4,    KC_P5,    KC_P6,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,    KC_PDOT),
+
+  /* Keymap _FL: Function Layer */
+  [_FL] = LAYOUT(
+    KC_TRNS,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_RMOD,  RGB_MOD,   _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,   _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,   QK_BOOT,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,  _______,
+    _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              RGB_VAI,  _______,  _______,  _______,  _______,
+    _______,  UC_WIN,   _______,                      _______,                                _______,  _______,  _______,   RGB_RMOD,  RGB_VAD,  RGB_MOD,  _______,  _______)
+};
+
+// All these numbers match up with the indexed positions in the keymap above
+const uint8_t NUM_LOCK_KEY_INDEX = 13;
+const uint8_t CAPS_LOCK_KEY_INDEX = 54;
+const uint8_t ALPHA_KEYS[] = { 
+  37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+   55, 56, 57, 58, 59, 60, 61, 62, 63,
+    71, 72, 73, 74, 75, 76, 77
+};
+const uint8_t NUM_PAD_KEYS[] = {
+  50, 51, 52,
+  67, 68, 69,
+  83, 84, 85,
+      97, 98
+};
+
+int clamp(int x, int lower, int upper) {
+  if(x > upper){
+    return upper;
+  }
+  if(x < lower){
+    return lower;
+  }
+  return x;
+}
+
+// vars for tracking the animation state
+double caps_flood_position = 0;
+uint32_t last_frame_time;
+
+// This is basically the amount of mills to wait between frames
+// Bigger number, slower the animation. 16 == 60fps
+const int FLOOD_SPEED_DIVISOR = 16;
+
+// This sets the order that the keys turn on, plus 2 variables to hold the array sizes
+// -1 is used as null because C doesn't like variable sized 2D arrays
+ // The first grouping is intentionally empty as that is where the lock key itself turns off/on
+const int CAPS_FLOOD_STEPS = 12;
+const int CAPS_FLOOD_HEIGHT = 3;
+const uint8_t ALPHA_KEY_GROUPS[12][3] = {
+  { -1, -1, -1 },
+  { 55, -1, -1 },
+  { 37, 56, 71 },
+  { 38, 57, 72 },
+  { 39, 58, 73 },
+  { 40, 59, 74 },
+  { 41, 60, 75 },
+  { 42, 61, 76 },
+  { 43, 62, 77 },
+  { 44, 63, -1 },
+  { 45, -1, -1 },
+  { 46, -1, -1 }
+};
+
+
+/**
+ * A helper function that can be called while capslock is on or animating to progress
+ * or maintain the state of the LEDs
+ */
+void update_cap_flood(void){
+  int flood_index = clamp(floor(caps_flood_position), 0, CAPS_FLOOD_STEPS);
+
+  // Get the inverted hue as RGB values
+  HSV hsv = {0, 255, 255};
+  hsv.h = (rgb_matrix_get_hue() + 128) % 256;
+  hsv.s = rgb_matrix_get_sat();
+  hsv.v = rgb_matrix_get_val();
+  RGB rgb = hsv_to_rgb(hsv);
+
+  rgb_matrix_set_color(CAPS_LOCK_KEY_INDEX, rgb.r, rgb.g, rgb.b);
+
+  for (int i = 0; i < flood_index; i++) {
+    for (int ii = 0; ii < CAPS_FLOOD_HEIGHT; ii++) {
+      uint8_t key = ALPHA_KEY_GROUPS[i][ii];
+      if (g_led_config.flags[key] & LED_FLAG_KEYLIGHT) {
+        if( key > -1 ){
+          rgb_matrix_set_color(key, rgb.r, rgb.g, rgb.b);
+        }
+      }
+    }
+  }
+}
+
+/**
+ * This is invoked once every cycle by the firmware and is used to set the Caps/Num/Scroll lock indicators
+ */
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+
+  // Use the time difference between invokes to set how far to advance the animation
+  uint32_t delta = timer_elapsed32(last_frame_time);
+  double flood_amount = (double) delta /  (double) FLOOD_SPEED_DIVISOR;
+  
+  // Deal with CapsLock
+  if (host_keyboard_led_state().caps_lock) {
+    if(caps_flood_position < 0){
+      caps_flood_position = 0;
+    }
+    if(caps_flood_position <= CAPS_FLOOD_STEPS){
+      caps_flood_position = caps_flood_position + flood_amount;
+    }
+    update_cap_flood();
+  } else {
+    if(caps_flood_position > CAPS_FLOOD_STEPS){
+      caps_flood_position = CAPS_FLOOD_STEPS;
+    }
+    if( caps_flood_position >= 0 ){
+      caps_flood_position = caps_flood_position - flood_amount;
+      update_cap_flood();
+    }
+  }
+
+  // Deal with NumLock
+  if (!host_keyboard_led_state().num_lock) {
+    rgb_matrix_set_color(NUM_LOCK_KEY_INDEX, RGB_RED);
+
+    int arrLen = sizeof NUM_PAD_KEYS / sizeof NUM_PAD_KEYS[0];
+    for (int i = 0; i < arrLen; i++) {
+        uint8_t key = NUM_PAD_KEYS[i];
+        if (g_led_config.flags[key] & LED_FLAG_KEYLIGHT) {
+            rgb_matrix_set_color(key, RGB_RED);
+        }
+    }
+  }
+  
+  last_frame_time = timer_read32();
+  return false;
+}
---
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/dispossible/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+CONSOLE_ENABLE = no
+
+# Optimisations
+LTO_ENABLE = yes
